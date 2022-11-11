8e4d03ff893abc2870b48f590ad2b15b853ac2ac
pradeepveeramaneni <pradeepraov26@gmail.com>
2022-11-11 12:06:24 -0600

    new changes

--- controllers/watch.js
+++ controllers/watch.js
@@ -70,27 +70,7 @@ exports.watch_delete = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-}; 
-
-
- 
-// Handle watch update form on PUT. 
-exports.watch_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: watch update PUT' + req.params.id);    
 };  
-
-// List of all watchs 
-exports.watch_update_put = async function(req, res) { 
-    try{ 
-        thewatchs = await watch.find(); 
-        res.send(thewatchs); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
-
 // VIEWS 
 // Handle a show all view 
 exports.watch_view_all_Page = async function(req, res) { 
@@ -126,3 +106,36 @@ exports.watch_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`);  
     }   
 };
+// for a specific Costume.
+exports.watch_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await watch.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   //Handle Costume update form on PUT.
+exports.watch_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await watch.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.watch_brand)
+ toUpdate.watch_brand = req.body.watch_brand;
+ if(req.body.watch_cost) toUpdate.watch_cost = req.body.watch_cost;
+ if(req.body.watch_color) toUpdate.watch_color = req.body.watch_color;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
+

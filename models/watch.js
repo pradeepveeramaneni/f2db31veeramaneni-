0bc51c76401c1f28d9213a698de212d5c42efb41
pradeepveeramaneni <pradeepraov26@gmail.com>
2022-11-29 23:45:32 -0600

    new changes

--- models/watch.js
+++ models/watch.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose") 
 const watchSchema = mongoose.Schema({ 
-    watch_brand: {type:String},     
+    watch_brand: {type:String,minLength: 3},     
     watch_color: {type:String},
     watch_cost: { type: Number, min: 10, max: 1000 }
 }) 
